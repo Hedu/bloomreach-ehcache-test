ccd07a33f92946b48a96a845ef9e74dc5113452f
Hedu <eperezghedu@gmail.com>
2020-02-21 11:24:59 +0100

    init project

---
+++ site/components/src/main/java/org/example/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.example.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="bloomreacharchetype13.3.0:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
